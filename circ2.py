c85f0792ee4fa354ce0b1bde5ce363aa106cf668
Ester Fiorillo <32225398+esterfiorillo@users.noreply.github.com>
2020-04-24 10:20:53 -0300

    Update circ2.py

--- circ2.py
+++ circ2.py
@@ -7,20 +7,8 @@ Created on Fri Aug  2 15:45:05 2019
 """
 
 import numpy as np
-import cv2
-i=0
-j=47
-k=0
-l=44
-img = np.zeros((1024,1024,3), np.uint8)
-for i in range (60):
-    j=4
-    for k in range (60):
-        cv2.circle(img,(l, j), 2, (0,0,255), -1)
-        j = j+10
-    l = l+10
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
-new_line
+def add (a, b)
+"""This is an add function"""
+    c = a+b
+    return c
